d0f036874fd40ac66389f6d719fae2df872abcc4
Jorge Alia <jorgealia94@gmail.com>
2022-07-04 16:14:56 +0200

    Add mongooseConnect and workout model

---
+++ src/models/workout.model.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { mongooseConnect } from '../db/mongoose.js';
+
+(async () => {
+    await mongooseConnect();
+})();
+
+export interface iWorkout {
+    _id?: string;
+    title: string;
+    image: string;
+    video: string;
+    description: string;
+    category: string;
+    complementaryMaterial: string;
+    favorite: boolean;
+    duration: number;
+    intensity: string;
+    comments?: Array<{ text: string; user: string; score: number }>;
+}
+
+const workoutSchema = new mongoose.Schema({
+    title: { type: mongoose.SchemaTypes.String, required: true },
+    image: { type: mongoose.SchemaTypes.String, required: true },
+    video: { type: mongoose.SchemaTypes.String, required: true },
+    description: mongoose.SchemaTypes.String,
+    category: mongoose.SchemaTypes.String,
+    complementaryMaterial: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+    },
+    favorite: {
+        type: mongoose.SchemaTypes.Boolean,
+        default: false,
+    },
+    duration: { type: mongoose.SchemaTypes.Number, required: true },
+    intensity: { type: mongoose.SchemaTypes.Number, required: true },
+    comments: [
+        {
+            text: { type: mongoose.SchemaTypes.String },
+            user: { type: mongoose.Types.ObjectId, ref: 'User' },
+            score: { type: mongoose.SchemaTypes.Number },
+        },
+    ],
+});
+
+workoutSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.__v;
+    },
+});
+
+export const Workout = mongoose.model('Workout', workoutSchema);
