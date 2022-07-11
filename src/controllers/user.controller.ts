a3437b4b96423d8ef70468d7db625054c9696ba6
Jorge Alia <jorgealia94@gmail.com>
2022-07-11 13:15:49 +0200

    Update method loginRegister

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -65,6 +65,7 @@ export class UserController<T> extends BasicController<T> {
         const findUser: any = await this.model.findOne({
             email: req.body.email,
         });
+
         if (!findUser || !aut.compare(req.body.passwd, findUser.passwd)) {
             const error = new Error('Invalid user or password');
             error.name = 'UserAuthorizationError';
@@ -78,7 +79,7 @@ export class UserController<T> extends BasicController<T> {
         const token = aut.createToken(tokenPayLoad);
         resp.setHeader('Content-type', 'application/json');
         resp.status(201);
-        resp.send(JSON.stringify({ token, id: findUser.id }));
+        resp.send(JSON.stringify({ token, user: findUser }));
     };
     addWorkoutController = async (
         req: Request,
